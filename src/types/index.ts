8ed5d34e6348d28820dc7bf3579d3c942b219b8d
anonymus-netizien <kollururamchandarrao@gmail.com>
2026-07-27 23:29:27 +0530

    feat(github): align frontend type definitions with backend DTOs

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export type { ApiResponse } from './api'
 export type { User, LoginRequest, LoginResponse, RegisterRequest, OtpVerifyRequest } from './auth'
-export type { GitHubConnection, GitRepository, Branch, ConnectionStatus, ModalStep } from './github'
+export type { GitHubConnection, GitHubStatusResponse, GitRepository, Branch, ConnectionStatus, ModalStep } from './github'
 export type { ThreadStatus, RiskFlag, ChangeThread, DecisionNote, ChangedFile, Commit } from './thread'
 export type { BriefStatus, Citation, BriefClaim, BriefSection, ChangeBrief } from './brief'
+
